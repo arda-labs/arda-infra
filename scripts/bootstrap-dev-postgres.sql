fcd690a392ba7844c06635f8ffbb7edc1615c900
HoanIT <lehoan.dev@gmail.com>
2026-05-01 15:35:54 +0700

    infra: add crm and media database bootstrap

--- scripts/bootstrap-dev-postgres.sql
+++ scripts/bootstrap-dev-postgres.sql
@@ -33,3 +33,37 @@ WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'notification')\gexe
 ALTER DATABASE notification OWNER TO notification;
 
 GRANT ALL PRIVILEGES ON DATABASE notification TO notification;
+
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'crm') THEN
+    CREATE ROLE crm LOGIN PASSWORD 'crm@123';
+  ELSE
+    ALTER ROLE crm LOGIN PASSWORD 'crm@123';
+  END IF;
+END
+$$;
+
+SELECT 'CREATE DATABASE crm OWNER crm'
+WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'crm')\gexec
+
+ALTER DATABASE crm OWNER TO crm;
+
+GRANT ALL PRIVILEGES ON DATABASE crm TO crm;
+
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'media') THEN
+    CREATE ROLE media LOGIN PASSWORD 'media@123';
+  ELSE
+    ALTER ROLE media LOGIN PASSWORD 'media@123';
+  END IF;
+END
+$$;
+
+SELECT 'CREATE DATABASE media OWNER media'
+WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'media')\gexec
+
+ALTER DATABASE media OWNER TO media;
+
+GRANT ALL PRIVILEGES ON DATABASE media TO media;
